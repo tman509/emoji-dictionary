99af71337492c5cbd0cc36d8924d7ed6b0532fe2
Kaitlyn Hawker <kaitglauser@Sandis-MacBook-Pro.local>
2017-01-06 20:14:25 -0700

    first commit

---
+++ ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Emoji Dictionary
+//
+//  Created by Kaitlyn Hawker on 1/6/17.
+//  Copyright © 2017 Kaitlyn Hawker. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
